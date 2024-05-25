ac32dae1b3b2b4a6a0c8678d261569ab86479066
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2024-05-25 15:22:06 -0700

    set up spring

---
+++ backend/gymgenie/src/main/java/com/gymgenie/gymgenie/GymgenieApplication.java
@@ -0,0 +1,13 @@
+package com.gymgenie.gymgenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymgenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymgenieApplication.class, args);
+	}
+
+}
