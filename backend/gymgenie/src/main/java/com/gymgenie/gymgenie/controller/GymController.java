8b02b94ec80b0cac8f4c7fb3006dfc9332dc2c94
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2024-05-25 17:37:30 -0700

    add vertex AI

--- backend/gymgenie/src/main/java/com/gymgenie/gymgenie/controller/GymController.java
+++ backend/gymgenie/src/main/java/com/gymgenie/gymgenie/controller/GymController.java
@@ -4,6 +4,7 @@ import com.gymgenie.gymgenie.service.GeminiApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +15,14 @@ public class GymController {
     private GeminiApiService externalApiService;
 
     @GetMapping("/fetch-data")
-    public String fetchData() {
-        return externalApiService.fetchDataFromExternalApi();
+    // public String fetchData() {
+    //     return externalApiService.fetchDataFromExternalApi();
+    // }
+
+    public String fetchData(@RequestParam int age,
+                        @RequestParam double height,
+                        @RequestParam double weight,
+                        @RequestParam String gender) {
+    return externalApiService.fetchDataFromExternalApi(age, height, weight, gender);
     }
 }
--- backend/gymgenie/src/main/java/com/gymgenie/gymgenie/service/GeminiApiService.java
+++ backend/gymgenie/src/main/java/com/gymgenie/gymgenie/service/GeminiApiService.java
@@ -4,15 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.api.GenerateContentResponse;
+import com.google.cloud.vertexai.generativeai.preview.ContentMaker;
+import com.google.cloud.vertexai.generativeai.preview.GenerativeModel;
+import com.google.cloud.vertexai.generativeai.preview.PartMaker;
+
 @Service
 public class GeminiApiService {
 
     @Autowired
     private RestTemplate restTemplate;
 
-    public String fetchDataFromExternalApi() {
-        // Replace "https://api.example.com/data" with the actual URL of the external API
-        String apiUrl = "https://swapi.dev/api/people/1/";
-        return restTemplate.getForObject(apiUrl, String.class);
-    }
+    // public String fetchDataFromExternalApi(int age, double height, double weight, String gender) {
+    //     // Replace "https://api.example.com/data" with the actual URL of the external API
+    //     String apiUrl = "https://swapi.dev/api/people/1/";
+    //     return restTemplate.getForObject(apiUrl, String.class);
+    // }
+    public String fetchDataFromExternalApi(int age, double height, double weight, String gender) {
+      // Replace "https://api.example.com/data" with the actual URL of the external API
+      try (VertexAI vertexAI = new VertexAI(projectId, location)) {
+        byte[] imageBytes = Base64.getDecoder().decode(dataImageBase64);
+
+        GenerativeModel model = new GenerativeModel("gemini-pro-vision", vertexAI);
+        GenerateContentResponse response = model.generateContent(
+            ContentMaker.fromMultiModalData(
+                "What is this image about?",
+                PartMaker.fromMimeTypeAndData("image/jpg", imageBytes)
+            ));
+
+        System.out.println(ResponseHandler.getText(response));
+        // return restTemplate.getForObject(apiUrl, String.class);
+      }
+  }
 }
